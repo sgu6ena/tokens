f0c4e436482eea96ac16d434a30fd980c77256fe
sgu6ena <issar@admin.idknet.com>
2022-06-29 16:46:21 +0300

    fix links

--- src/components/layout/menu/Menu.tsx
+++ src/components/layout/menu/Menu.tsx
@@ -1,23 +1,27 @@
 import {FC} from 'react';
 import {headerMenu} from "./menu.data";
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 
 import styles from "./Menu.module.scss"
+import classNames from "classnames";
+
 
 const Menu: FC = () => {
-  return (
-    <ul className={styles.list}>
-      {headerMenu.map(item => (
-        <li key={item.link} className={styles.item}>
-          <NavLink to={item.link}
-                   className={isActive => isActive ? "underline" : styles.inactive}
-          
-          >
-            {item.title}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
+  let {pathname} = useLocation();
+  
+  return (<>{}
+      <ul className={styles.list}>
+        {headerMenu.map(item => (
+          <li key={item.link} className={styles.item}>
+            <NavLink to={item.link}
+                     className={item.link === pathname ? classNames(styles.active) : classNames(styles.inactive)}
+                     end>
+              {item.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
